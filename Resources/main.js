bf85595786f91e432664e2bb26e2ea378eaa4040
Robert <pinneyr@hotmail.com>
2023-08-24 19:18:19 +0200

    Trying to connect js

---
+++ Resources/main.js
@@ -0,0 +1,16 @@
+/*
+const newText = () => {
+    let paragraph = document.createElement('p');
+paragraph.id = 'cogwheel-dad-joke-generator';
+paragraph.innerHTML = 'Please please please let this work'
+document.cogwheelDadJokeGenerator.appendChild(paragraph);
+};
+
+cogwheelDadJokeGenerator.onclick = newText;
+*/
+
+const heading = document.getElementsByTagName('h1');
+heading = "<h2>This is a test</h2>";
+console.log(heading);
+
+
