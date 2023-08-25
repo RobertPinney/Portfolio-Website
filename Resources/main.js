bdc5cc8136328bdea4d27ed42e435cd46a3c1053
Robert <pinneyr@hotmail.com>
2023-08-25 08:54:37 +0200

    first succesful JS test

--- Resources/main.js
+++ Resources/main.js
@@ -8,9 +8,25 @@ document.cogwheelDadJokeGenerator.appendChild(paragraph);
 
 cogwheelDadJokeGenerator.onclick = newText;
 */
-
+/*
 const heading = document.getElementsByTagName('h1');
 heading = "<h2>This is a test</h2>";
 console.log(heading);
+*/
+//document.body.innerHTML = '<h2>This is a test!</h2>';
+/*
+let eventTarget= document.querySelector('h1');
+
+synopsis.onclick = () => {
+    eventTarget.style.color = 'red';
+};
 
 
+synopsis.addEventListener('click', turnRed);
+*/
+
+let turnRed = document.querySelector('.synopsis');
+//turnRed.style.color = 'red'
+turnRed.onclick = () => {
+    turnRed.style.color = 'red';
+};
