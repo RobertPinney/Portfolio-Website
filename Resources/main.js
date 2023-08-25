eeb4c18da05dce9403fe708933fe5299a49a4304
Robert <pinneyr@hotmail.com>
2023-08-25 17:22:13 +0200

    JS complete

--- Resources/main.js
+++ Resources/main.js
@@ -1,32 +1,45 @@
-/*
-const newText = () => {
-    let paragraph = document.createElement('p');
-paragraph.id = 'cogwheel-dad-joke-generator';
-paragraph.innerHTML = 'Please please please let this work'
-document.cogwheelDadJokeGenerator.appendChild(paragraph);
-};
+/* Four functions for each cogwheel to turn red and produce text */
 
-cogwheelDadJokeGenerator.onclick = newText;
-*/
-/*
-const heading = document.getElementsByTagName('h1');
-heading = "<h2>This is a test</h2>";
-console.log(heading);
-*/
-//document.body.innerHTML = '<h2>This is a test!</h2>';
-/*
-let eventTarget= document.querySelector('h1');
-
-synopsis.onclick = () => {
-    eventTarget.style.color = 'red';
+let dadJoke = document.querySelector('#cogwheel-dad-joke-generator');
+dadJoke.onclick = () => {
+    dadJoke.style.color = 'red';
+    teaCozy.style.color = 'black';
+    originalHomepage.style.color = 'black';
+    getBaked.style.color = 'black';
+    let dadJokeText = document.querySelector('#generated-text');
+    dadJokeText.innerHTML=`Push the big button and receive your dad joke of the day!`;
 };
 
 
-synopsis.addEventListener('click', turnRed);
-*/
-
-let turnRed = document.querySelector('.synopsis');
-//turnRed.style.color = 'red'
-turnRed.onclick = () => {
-    turnRed.style.color = 'red';
+let getBaked = document.querySelector('#baked-goods');
+getBaked.onclick = () => {
+    getBaked.style.color = 'red';
+    teaCozy.style.color = 'black';
+    originalHomepage.style.color = 'black';
+    dadJoke.style.color = 'black';
+    let getBakedText = document.querySelector('#generated-text');
+    getBakedText.innerHTML=`Excluding a very basic homepage in HTML and CSS, this is my very first project. I created this webpage, and focused a good deal on using Flexbox for the first time.`;
 };
+
+let teaCozy = document.querySelector('#tea-cozy');
+teaCozy.onclick = () => {
+    teaCozy.style.color = 'red';
+    originalHomepage.style.color = 'black';
+    getBaked.style.color = 'black';
+    dadJoke.style.color = 'black';
+    let teaCozyText = document.querySelector('#generated-text');
+    teaCozyText.innerHTML=`Static website built using basic instructions to simulate real-life experience.`;
+};
+
+let originalHomepage = document.querySelector('#original-homepage');
+originalHomepage.onclick = () => {
+    originalHomepage.style.color = 'red';
+    teaCozy.style.color = 'black';
+    getBaked.style.color = 'black';
+    dadJoke.style.color = 'black';
+    let originalHomepageText = document.querySelector('#generated-text');
+    originalHomepageText.innerHTML=`Original homepage. It looks terrible now, but I was so proud at the time. :-)`;
+};
+
+// Taking user back to top
+const toTop = () => window.scrollTo({top: 0, behavior: 'smooth'});
